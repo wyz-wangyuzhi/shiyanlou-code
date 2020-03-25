6cddc582d4ccc3591c7c0bebe24f54621ae52176
wangyuzhi <84134514@qq.com>
2020-03-25 15:09:41 +0800

    Third commit

---
+++ jump7.py
@@ -0,0 +1,11 @@
+total = 99
+for  number in range(1,100):
+    if number  %7==0:
+        continue
+    else:
+        string =str(number)
+        if string.endswith("7"):
+            continue
+    total -= 1
+print("从1数到99拍腿",total,"")
+
